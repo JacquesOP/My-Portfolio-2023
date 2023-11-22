2b153b20ecda0b3ef1d4d16c840e36c7ad2c4e22
JacquesOP <jacquesoprince.pro@icloud.com>
2023-11-22 10:53:32 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/*===== GSAP ANIMATION =====*/
+
+// HOME
+gsap.from(".header", {opacity: 0, duration: 1, delay: 2, x:30, stagger: 0.2});
+gsap.from(".burger", {opacity: 0, duration: 1, delay: 2, x:30, stagger: 0.2});
+gsap.from(".home__title", {opacity: 0, duration: 1, delay: 1.4, x:-60, stagger: 0.2});
+gsap.from(".home__title2", {opacity: 0, duration: 1, delay: 1.8, y:30, stagger: 0.2});
+gsap.from(".portfolio__updates-h2", {opacity: 0, duration: 1, delay: 1.6, x:-40, stagger: 0.2});
+gsap.from(".portfolio__updates-h3", {opacity: 0, duration: 1, delay: 1.9, y:30, stagger: 0.2});
+gsap.from(".catch", {opacity: 0, duration: 1, delay: 1.9, y:30, stagger: 0.2});
